6cfda8afb0b1c82388191ebe88767f917d4c2fab
GalinaUm <GalinaUmerzakhova@yandex.ru>
2026-02-19 13:04:39 +0300

    message_views

--- web_app/views.py
+++ web_app/views.py
@@ -1,9 +1,8 @@
 from django.urls import reverse_lazy, reverse
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
-from .forms import MailRecipientForm
+from .forms import MailRecipientForm, MessageForm
 
-
-from web_app.models import MailRecipient
+from web_app.models import MailRecipient, Message
 
 
 class BaseView(TemplateView):
@@ -46,5 +45,33 @@ class MailRecipientUpdateView(UpdateView):
 class MailRecipientDeleteView(DeleteView):
     model = MailRecipient
     success_url = reverse_lazy('MailRecipientListView')
+
+
+class MessageListView(ListView):
+    model = Message
+
+class MessageDetailView(DetailView):
+    model = Message
+    def get_object(self, queryset = None):
+        self.object = super().get_object(queryset)
+        self.object.save()
+        return self.object
+
+class MessageCreateView(CreateView):
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy('MessageListView')
+
+class MessageUpdateView(UpdateView):
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy('MessageListView')
+
+    def get_success_url(self):
+        return reverse('web_app:message_detail', args=[self.kwargs.get('pk')])
+
+class MessageDeleteView(DeleteView):
+    model = Message
+    success_url = reverse_lazy('MessageListView')
 
 
